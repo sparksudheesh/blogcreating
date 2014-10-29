e147054a4554b7c04c7d21b8b53fa5d3f66449ca
shinu <oshinu@sparksupport.com>
2014-10-29 10:03:46 +0530

    test

--- blogs/tests.py
+++ blogs/tests.py
@@ -1,27 +1,24 @@
+import unittest
 from django.test import TestCase, Client
 from blogs.models import Post
 from django.contrib.auth.models import User
+from datetime import datetime
 import json
 
 class BlogTest(TestCase):
 
     def setUp(self):
-        self.c = Client()
+        self.client = Client()
  
     def test_index(self):
         user=User.objects.create(username="abc")
-        Post.objects.create(title="title1", slug="one", author=user, content="hai hello",public=1, created="2014-10-12 00:00:00", updated="2014-10-12 00:00:00", published="2014-10-12 00:00:00")
+        response = self.client.get('/blogs/')
+        response = self.client.post('/blogs/', {'title':'title1','slug': 'one', 'author':'user', 'content':'hai hello','public':'1', 'created': '2014-10-12 00:00:00', 'updated': '2014-10-12 00:00:00', 'published':'2014-10-12 00:00:00'  })
         
-    def test_one(self):
-         self.c = Client()
-         user=User.objects.create(username="abc")
-         self.c.get('/blogs/1/')
-         response = self.c.post('/blogs/1/', {'title': 'title1', 'slug': 'one','author':'user', 'content':'hai hello','public':'1', 'created':'2014-10-12 00:00:00', 'updated':'2014-10-12 00:00:00', 'published':'2014-10-12 00:00:00'})
-         #Post.objects.create(title="title1", slug="one", author=user, content="hai hello",public=1, created="2014-10-12 00:00:00", updated="2014-10-12 00:00:00", published="2014-10-12 00:00:00")
-
+    def test_edit(self):
+        user=User.objects.create(username="abc")
+        response = self.client.get('/blogs/1/')
+        response = self.client.post('/blogs/1/', { 'title':'hai','slug':'two','author':'user','content':'hai','public':'1','created':'2014-10-29 00:00:00', 'updated': '2014-10-29 00:00:00', 'published':'2014-10-29 00:00:00'  })
         
-
         
 
-      
-        
--- users/tests.py
+++ users/tests.py
@@ -1,8 +1,10 @@
+import unittest
 from django.test import TestCase, Client
 from users.models import Profile
 from django.contrib.auth.models import User
+from datetime import datetime
+import json
 
-# Create your tests here.
 class UserTest(TestCase):
     
     def setUp(self):
@@ -10,14 +12,14 @@ class UserTest(TestCase):
 
     def test_index(self):
         user=User.objects.create(username="zzz")
-        profile=Profile.objects.create(user=user)
+        response = self.client.get('/users/')
+        response = self.client.post('/users/', { 'username': '1'})
 
-    def test_one(self):
-         self.c = Client()
-         user=User.objects.create(username="abc")
-         profile=Profile.objects.create(user=user)
-         self.c.get('/users/1/')
-         response = self.c.post('/users/1/', {'user': 'xyz'})
+    def test_edit(self):
+        user=User.objects.create(username="abc")
+        response = self.client.get('/users/1/')
+        response = self.client.post('/users/1/', { 'username':'2'})
+        
         
 
         
